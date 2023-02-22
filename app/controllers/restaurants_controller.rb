5f6163cfe280326e0bcf2f825a5385aae8c10a88
Janett Gerrard <mikanchik@gmail.com>
2023-02-22 12:56:53 +0000

    index, show, new, create working for restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,6 +12,14 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
 
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 end
